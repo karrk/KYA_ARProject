ea249a6f4a4a870da578997b9c8b90385d5a4a52
karrk <101902544+karrk@users.noreply.github.com>
2024-10-08 16:27:03 +0900

    [Feature] 게임 플레이 영역 공간 위치, 각도 조정 기능 추가

--- Assets/1.Scripts/Managers/DataManager.cs
+++ Assets/1.Scripts/Managers/DataManager.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class DataManager
 {
-    public Vector3 GroundSize = new Vector3(5, 5,5);
+    public Vector3 GroundSize = new Vector3(0.5f,0.5f,0.5f);
+
+    public Vector3 GroundPos;
 }
