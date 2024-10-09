a84a574fcdd5a1ac496c0b6df1bd5274303db37b
karrk <101902544+karrk@users.noreply.github.com>
2024-10-09 09:29:13 +0900

    [Feature] 컨트롤러, 캐릭터 연결

--- Assets/1.Scripts/Managers/Manager.cs
+++ Assets/1.Scripts/Managers/Manager.cs
@@ -5,6 +5,14 @@ public class Manager : MonoBehaviour
     private static Manager _instance = null;
     public static Manager Instance => _instance;
 
+    [SerializeField] private PlayerController _player;
+    public PlayerController Player => _player;
+
+    public JoyStick JoyStick;
+
+    private bool _playMode;
+    public bool PlayMode => _playMode;
+
     private void Awake()
     {
         if(_instance == null)
@@ -26,5 +34,23 @@ public class Manager : MonoBehaviour
 
             return _data;
         }
+    }
+
+    public void SetPlayer(PlayerController m_player)
+    {
+        if(this._player != null)
+        {
+            Destroy(m_player.gameObject);
+            Debug.LogError("플레이어가 이미 존재");
+            return;
+        }
+
+        this._player = m_player;
+        JoyStick.OnInputed += m_player.Move;
+    }
+
+    public void SetPlayMode(bool m_active)
+    {
+        this._playMode = m_active;
     }
 }
